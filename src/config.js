fc472ebcb9649871783f55fffe64dea71d45d80d
nataliesun <natsun121@gmail.com>
2019-06-20 21:23:24 -0700

    changed config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api' || 'https://dry-shore-81114.herokuapp.com/api',
+    API_ENDPOINT: 'https://dry-shore-81114.herokuapp.com/api' || 'http://localhost:8000/api',
     TOKEN_KEY: 'estatecloud-client-auth-token',
 }
