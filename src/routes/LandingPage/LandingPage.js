613b0a85a9165b82f6b7855fc21fb809fa5fc353
nataliesun <natsun121@gmail.com>
2019-07-01 09:22:55 -0700

    some styles

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -11,12 +11,16 @@ class LandingPage extends Component {
   render() {
     return (
       <div className="LandingPage">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <polygon className="svg--sm" fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100" />
+          <polygon className="svg--lg" fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+        </svg> */}
         <img src={schedule} alt="schedule" className="hero" />
         <section>
           <h3>Get organized</h3>
           <div className="description">
             <img src={collecting} alt="collecting" />
-            <p>EstateCloud helps you visualize all your properties in one place, so you never miss an important date.</p>
+            <p>Keep all your properties information in one place, so you never miss an important date.</p>
           </div>
         </section>
         <section>
@@ -30,7 +34,7 @@ class LandingPage extends Component {
           <h3>Connect with your friends</h3>
           <div className="description">
             <img src={calendar} alt="calendar" />
-            <p>Interactive calendars to help you keep track of who's staying when and where.</p>
+            <p>Interactive calendars to track who's staying when and where.</p>
           </div>
         </section>
 
