3c48302f042c8380ba4a081a63d0ea33113bf147
nataliesun <natsun121@gmail.com>
2019-07-17 13:57:06 -0700

    favicon

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://estatecloud-server.herokuapp.com/api',
+    API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'estatecloud-client-auth-token',
 }
 
